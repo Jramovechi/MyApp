62a36d5288e569774fa08d47234286beb1e21efd
Jramovechi <jonathanramovechi@gmail.com>
2022-01-23 09:30:45 -0300

    2do Desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <nav className="Nav">
+            <ul>
+                <li>Home</li>
+                <li>Galeria</li>
+                <li>Info</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
